fdfbe90600d84513a9b8903d9cbfa9e39af3aa4e
Demian Tejo <demian.tejo+github@gmail.com>
2018-08-25 20:56:23 -0300

    add localized amount

--- Let's Calc/Let's Calc/StringExtension.swift
+++ Let's Calc/Let's Calc/StringExtension.swift
@@ -16,6 +16,27 @@ extension String {
         return formatter.number(from: self)?.doubleValue
     }
     
+    func localizedAmount() -> String {
+        let usLocale = Locale(identifier: "en_US")
+        let usDecimalSeparator = usLocale.decimalSeparator!
+        let usGroupingSeparator = usLocale.groupingSeparator!
+        let formatter = NumberFormatter()
+        formatter.locale = usLocale
+        formatter.numberStyle = NumberFormatter.Style.decimal
+        formatter.usesGroupingSeparator = true
+        guard let number = self.toDouble() else{
+            return self
+        }
+        guard let result = formatter.string(from: number as NSNumber) else {
+            return self
+        }
+        guard let decimalSeparator = Locale.current.decimalSeparator , let groupingSeparator = Locale.current.groupingSeparator else {
+            return result
+        }
+        
+        return result.replacingOccurrences(of: usDecimalSeparator, with: "@").replacingOccurrences(of: usGroupingSeparator, with: groupingSeparator).replacingOccurrences(of: "@", with: decimalSeparator)
+    }
+    
     func mathExpresionResult()-> String{
         var mathExpresion = self.replacingOccurrences(of: " ", with: "")
         mathExpresion = mathExpresion.replacingOccurrences(of: "÷", with: "/")
